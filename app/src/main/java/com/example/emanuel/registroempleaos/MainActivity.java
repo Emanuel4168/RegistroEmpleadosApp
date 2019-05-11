a8a279fad50dee0d22da5ac7854eb241cf0e851f
Emanuel4168 <emanuel.l4168@gmail.com>
2019-05-11 11:40:09 -0600

    Creación de la actividad consult y del paquete entities, que contiene la calse employee

--- app/src/main/java/com/example/emanuel/registroempleaos/MainActivity.java
+++ app/src/main/java/com/example/emanuel/registroempleaos/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.emanuel.registroempleaos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText txtID,txtName,txtAge;
+    Button btnSave,btnConsult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtID = (EditText) findViewById(R.id.txtID);
+        txtName = (EditText) findViewById(R.id.txtName);
+        txtAge = (EditText) findViewById(R.id.txtAge);
+
+        btnSave = (Button) findViewById(R.id.btnSave);
+        btnConsult = (Button) findViewById(R.id.btnConsult);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == btnSave){
+
+            return;
+        }
+        if(view == btnConsult){
+
+            return;
+        }
     }
 }
---
+++ app/src/main/java/com/example/emanuel/registroempleaos/entities/Employee.java
@@ -0,0 +1,31 @@
+package com.example.emanuel.registroempleaos.entities;
+
+public class Employee {
+    private int edad;
+    private String name;
+    private String id;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
